f8e1108bd04f2226587a575a55dd3b378f677338
psilospore <syedjafri99@gmail.com>
2024-08-27 23:45:05 -0400

    Use tuple format for game

--- test/test_game.py
+++ test/test_game.py
@@ -4,5 +4,7 @@ from shieldpy.automata.game import SafetyGame, Transition
 from shieldpy.automata.product import product
 
 def test_product(simple_nfa, simple_nfa_2):
-    game = product(simple_nfa, simple_nfa_2)
+    nfa1, _, _ = simple_nfa
+    nfa2, _, _ = simple_nfa_2
+    game = product(nfa1, nfa2)
     assert len(game.states) == 9
