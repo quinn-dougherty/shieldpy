f1d8e2d04f3868f91a60dbaa2069ca3361227b86
psilospore <syedjafri99@gmail.com>
2024-09-01 22:29:18 -0400

    Use figure 5 instead. Create state and transitions progrrammatically.

--- src/shieldpy/environments/watertank.py
+++ src/shieldpy/environments/watertank.py
@@ -2,9 +2,7 @@ from typing import NewType
 from shieldpy.automata.nondeterministic_finite import NFA, Transition
 from enum import Enum
 
-# Water Tank Example from Figure 4
-# TODO do we want figure 4 or 5? Pretty sure figure 4
-# TODO move this to something like examples/
+# Water Tank Example from Figure 5
 
 ## PROMPT (TODO prompt is sparse here probably include more details in the prompt)
 ## https://arxiv.org/pdf/2306.12672 See Figure 13 for prompting example for world model generation.
@@ -17,63 +15,54 @@ Level = NewType('Level', int)
 minLevel = Level(0)
 maxLevel = Level(100)
 
-WaterTankState = Enum("WaterTankState", ["q_a", "q_b", "q_c", "q_d", "q_e", "q_f"])
+WaterTankState = Enum("WaterTankState", [f"q_{i}" for i in range(99)])
 
-# An "Ok level" is when 1 <= Level <= 99
-WaterTankAlphabet = Enum("WaterTankAlphabet", ["CLOSE_AND_OK_LEVEL", "OPEN_AND_OK_LEVEL"])
+# Do I need the * self loop?
+WaterTankAlphabet = Enum("WaterTankAlphabet", [f"(open, {i} <= level < {i+1})" for i in range(1, 99)] + [f"(close, {i} <= level < {i + 1})" for i in range(99)])
 
-# TODO rest of transition
-transitions = frozenset([
-    Transition(
-        WaterTankState.q_a,
-        WaterTankAlphabet.CLOSE_AND_OK_LEVEL,
-        WaterTankState.q_a
-    ),
-    Transition(
-        WaterTankState.q_a,
-        WaterTankAlphabet.OPEN_AND_OK_LEVEL,
-        WaterTankState.q_b
-    ),
-    Transition(
-        WaterTankState.q_b,
-        WaterTankAlphabet.OPEN_AND_OK_LEVEL,
-        WaterTankState.q_c
-    ),
-    Transition(
-        WaterTankState.q_c,
-        WaterTankAlphabet.OPEN_AND_OK_LEVEL,
-        WaterTankState.q_d
-    ),
-    Transition(
-        WaterTankState.q_d,
-        WaterTankAlphabet.OPEN_AND_OK_LEVEL,
-        WaterTankState.q_d
-    ),
-    Transition(
-        WaterTankState.q_d,
-        WaterTankAlphabet.CLOSE_AND_OK_LEVEL,
-        WaterTankState.q_e
-    ),
-    Transition(
-        WaterTankState.q_e,
-        WaterTankAlphabet.CLOSE_AND_OK_LEVEL,
-        WaterTankState.q_f
-    ),
-    Transition(
-        WaterTankState.q_f,
-        WaterTankAlphabet.CLOSE_AND_OK_LEVEL,
-        WaterTankState.q_a
-    )
-])
+
+# TODO going to make transitions programatically
+transitions = set()
+
+# Transition 1 step forward
+for i in range(98):
+    transitions.add(Transition(
+        getattr(WaterTankState, f"q_{i}"),
+        getattr(WaterTankAlphabet, f"(open, {i} <= level < {i+1})"),
+        getattr(WaterTankState, f"q_{i+1}")
+    ))
+
+# Transition 2 steps forward
+for i in range(97):
+    transitions.add(Transition(
+        getattr(WaterTankState, f"q_{i}"),
+        getattr(WaterTankAlphabet, f"(close, {i} <= level < {i+2})"),
+        getattr(WaterTankState, f"q_{i+2}")
+    ))
+
+# TODO are there n steps forward transitions?
+
+# Transition 1 step backward
+
+for i in range(1, 99):
+    transitions.add(Transition(
+        getattr(WaterTankState, f"q_{i}"),
+        getattr(WaterTankAlphabet, f"(close, {i-1} <= level < {i})"),
+        getattr(WaterTankState, f"q_{i-1}")
+    ))
+
+# TODO diagram doesn't show n > 1 steps backward but double check
+
+# TODO * loops
 
 watertank_nfa = NFA(
     states = WaterTankState,
-    transitions = transitions,
-    start = WaterTankState.q_a,
+    transitions = frozenset(transitions),
+    start = WaterTankState.q_0,
     accept = frozenset([e for e in WaterTankState]),
     alphabet= WaterTankAlphabet
 )
 
 # TODO create LTL spec
 
-# Now define a world model NFA for [INSERT ]
+# Now define a world model NFA for [INSERT ] the above is completely unrelated
