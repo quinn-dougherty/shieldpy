7766947725027957f694c9be0e1fe3bb358b3c6f
psilospore <syedjafri99@gmail.com>
2024-10-03 22:16:08 -0400

    LTL statement

--- haskell/src/Lib.hs
+++ haskell/src/Lib.hs
@@ -20,8 +20,9 @@ type P = (S, A) -> Set (S, Float)
 -- | Reward funtion
 type R = (S, A, S) -> Float
 
+
 -- | Markov Decision Process
-data MDP = MDP {
+data M = M {
   _S :: Set S
   , sᵢ :: S -- Initial state
   , _A :: Set A -- Finite set of boolean actions
@@ -30,37 +31,38 @@ data MDP = MDP {
 }
 
 -- Automata States
-data Q = Q Int
+data Q = Q Int deriving (Show, Eq, Ord)
 
 -- Transition
 type Transition _Σᵢ = (Q, _Σᵢ) → _Σᵢ
 
--- A finite state reactive system
-data S q _Σᵢ _Σ₀ = S {
+-- S: A finite state reactive system
+data Shield _Σᵢ _Σ₀ = Shield {
   _Q :: Set Q
   , q₀ :: Q
   , _Σᵢ :: _Σᵢ
   , _Σ₀ :: _Σ₀
-  , δ :: Transition q _Σᵢ
+  , δ :: Transition _Σᵢ
+  -- TODO , λ
   }
 
 -- Safety automation φˢ
 data SafetyAutomation _Σ = SafetyAutomation {
   q :: Set Q
   , q₀:: Q
-  , sigma :: _Σ
-  , δ :: Transition Q _Σ
+  , _Σ :: _Σ
+  , δ :: Transition _Σ
   , _F :: Set Q -- Set of safe states
   }
 -- | Used for synthesizing the shield
 -- Defines a set Fᵍ  ⊆ G of safe states
 -- Where win(g₀, g₁, ...) iff ∀i ≥ 0. gᵢ ∈ Fᵍ
 -- I think we can solve for this using SMT. In the original implementation they used BDDs.
-safetyGame ∷ G -> G
-safetyGame g = undefined
+-- safetyGame ∷ G -> G
+-- safetyGame g = undefined
 
   -- | Uses the safety game to synthesize a shield which implements the winning strategy in a new reactive system (Is it a finite reactive system?)
-shield ∷ S
+shield  ∷ Shield _Σᵢ _Σ₀
 shield =
     let g = undefined
         q = undefined
@@ -68,4 +70,80 @@ shield =
         _Σₒ= undefined
         δ' = undefined -- (g, σᵢ ) = (g, σᵢ, ρ(g, σᵢ))
         ρ = undefined
-    in S q _Σᵢ _Σₒ δ'
+    in undefined -- TODO S q _Σᵢ _Σₒ δ'
+
+type Σ = Set Int
+
+-- L but maybe just for preemptive shields
+-- e.g.  {level < 1, 1 ≤ level ≤ 99, level > 99}
+-- type Σᵢ¹ = L
+
+-- A but maybe just for preemtive shields
+-- e.g. {Open, Close}
+type Σᵢ² = A
+
+-- | Propositions TODO not sure how to represent this
+type Prop = String
+
+-- | LTL Formula data type
+data LTL
+  = AP Prop          -- Atomic proposition
+  | Not LTL
+  | And LTL LTL
+  | Or LTL LTL
+  | Implies LTL LTL
+  | X LTL            -- Next
+  | G LTL            -- Globally/Always
+  | F LTL            -- Eventually
+  | U LTL LTL        -- Until
+  deriving (Eq, Show)
+
+-- | Sugar
+(&&&) = And
+(|||) = Or
+(-->) = Implies
+
+-- Example in paper
+exampleLtlFormula :: LTL
+exampleLtlFormula = G ( AP "r" ||| X (AP "g"))
+
+waterTankExampleLtlFormula = G (AP "level > 0") &&& G (AP "level < 100") &&&
+                                -- TODO G ((AP "open" &&& X (AP "close")) --> (X X (AP "close") &&& XXX (AP "close")))
+
+-- | A trace I think?
+type Trace = [Set Prop]
+
+-- | Does the trace satisfy the LTL
+-- TODO LLM generated might be wrong. This tracks the index but I think we can write this recursively?
+satisfies :: Trace -> Int -> LTL -> Bool
+satisfies σ idx formula = case formula of
+  AP p ->
+    idx < length σ && p `elem` (σ !! idx)
+  Not f ->
+    not (satisfies σ idx f)
+  And f1 f2 ->
+    satisfies σ idx f1 && satisfies σ idx f2
+  Or f1 f2 ->
+    satisfies σ idx f1 || satisfies σ idx f2
+  Implies f1 f2 ->
+    if satisfies σ idx f1 then satisfies σ idx f2 else True
+  X f ->
+    satisfies σ (idx + 1) f
+  G f ->
+    all (\i -> satisfies σ i f) [idx .. length σ - 1]
+  F f ->
+    any (\i -> satisfies σ i f) [idx .. length σ - 1]
+  U f1 f2 ->
+    let future = drop idx σ
+        holdsAt i = satisfies σ i f2 && all (\j -> satisfies σ j f1) [idx .. i - 1]
+    in any (\i -> holdsAt i) [idx .. length σ - 1]
+
+-- | Preemptive Shield iteration
+-- Given the time step compute a set of safe actions (remove unsafe actions)
+-- Environment executes one action and moves to the next state providing a reward
+-- I think the paper describes this more clearly even though this might be harder to use
+-- The input alphabet Σᵢ = Σᵢ¹ x Σᵢ² = L x A
+-- The output alphabet Σₒ = 2ᴬ
+-- Where A is the boolean 'Actions' in the MDP
+-- preemptiveShieldIter :: (L, A) -> Set A
+-- preemptiveShieldIter (l, a) = undefined
